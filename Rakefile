5531a383b5abccfa3ff06e4150469f551dae44a1
wtnabe <wtnabe@gmail.com>
2013-01-20 18:33:12 +0900

    add : support starting simplecov

--- Rakefile
+++ Rakefile
@@ -35,9 +35,13 @@ RSpec::Core::RakeTask.new(:spec) do |spec|
   spec.pattern = FileList['spec/**/*_spec.rb']
 end
 
-RSpec::Core::RakeTask.new(:rcov) do |spec|
+RSpec::Core::RakeTask.new('spec:coverage') do |spec|
   spec.pattern = 'spec/**/*_spec.rb'
-  spec.rcov = true
+  if RUBY_VERSION < '1.9'
+    spec.rcov = true
+  else
+    ENV['COVERAGE'] = 'true'
+  end
 end
 
 task :default => :spec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,13 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'jshint4r'
+require 'simplecov' if RUBY_VERSION >= '1.9'
+
+if defined? SimpleCov and ENV['COVERAGE']
+  SimpleCov.start do
+    add_filter '/vendor/'
+  end
+end
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
